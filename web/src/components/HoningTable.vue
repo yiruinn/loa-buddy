a164d9006c655c1d88e7925a9a5b986cd961f224
Yirui Zhao <zhao106@illinois.edu>
2023-08-04 10:50:17 -0700

    add special hone efficiency calculator

---
+++ web/src/components/HoningTable.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="table-container">
+        <DataTable :value="items">
+            <Column field="level" header="Level"></Column>
+            <Column :field="calculateGoldCost" header="Total Cost"></Column>
+            <Column :field="calculateGoldPerSpecial" header="Cost Per"></Column>
+        </DataTable>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const props = defineProps({
+    items: Object,
+    calculateGoldCost: Function,
+    calculateGoldPerSpecial: Function,
+})
+
+</script>
--- web/src/views/SpecialHoning.vue
+++ web/src/views/SpecialHoning.vue
@@ -1,3 +1,97 @@
 <template>
-    <p>Special honing page</p>
+  <div id="app">
+    <h1>Special Honing</h1>
+    <h2>Prices</h2>
+    <div class="formgroup-inline">
+      <div class="field">
+        <label for="firstname3">Honor Shards (/500)</label>
+        <InputText id="firstname3" style="width:200px" v-model="honorShardsPrice" placeholder="Enter value" />
+      </div>
+      <div class="field">
+        <label for="firstname3">Guardian Stones (/10)</label>
+        <InputText id="firstname3" style="width:200px" v-model="gstonesPrice" placeholder="Enter value" />
+      </div>
+      <div class="field">
+        <label for="firstname3">Destruction Stones (/10)</label>
+        <InputText id="firstname3" style="width:200px" v-model="dstonesPrice" placeholder="Enter value" />
+      </div>
+      <div class="field">
+        <label for="firstname3">Fusion</label>
+        <InputText id="firstname3" style="width:200px" v-model="fusionPrice" placeholder="Enter value" />
+      </div>
+      <div class="field">
+        <label for="firstname3">Leapstones</label>
+        <InputText id="firstname3" style="width:200px" v-model="leapsPrice" placeholder="Enter value" />
+      </div>
+    </div>
+    <hr>
+    <h2>Gold Efficiency Per Special (1490)</h2>
+    <div class="formgrid grid">
+      <div class="field col">
+        <HoningTable :items="armorLevels" :calculateGoldCost="calculateGoldCost"
+          :calculateGoldPerSpecial="calculateGoldPerSpecial" />
+      </div>
+      <div class="field col">
+        <HoningTable :items="weaponLevels" :calculateGoldCost="calculateGoldCost"
+          :calculateGoldPerSpecial="calculateGoldPerSpecial" />
+      </div>
+    </div>
+
+  </div>
 </template>
+  
+<script setup>
+import { ref, computed } from 'vue';
+import InputText from 'primevue/inputtext';
+
+import HoningTable from '../components/HoningTable.vue';
+import jsonData from '../data/honing_brel.json';
+
+import '/node_modules/primeflex/primeflex.css'
+
+const honorShardsPrice = ref();
+const fusionPrice = ref();
+const gstonesPrice = ref();
+const dstonesPrice = ref();
+const leapsPrice = ref();
+
+const armorLevels = computed(() => jsonData.filter(item => item.level.includes('Armor')));
+const weaponLevels = computed(() => jsonData.filter(item => item.level.includes('Weapon')));
+
+const calculateGoldCost = (item) => {
+  return (
+    item.honor_shards * honorShardsPrice.value / 500 +
+    item.fusion * fusionPrice.value +
+    (item.gstones ?? 0) * gstonesPrice.value / 10 +
+    (item.dstones ?? 0) * dstonesPrice.value / 10 +
+    item.leaps * leapsPrice.value
+  );
+};
+
+const calculateGoldPerSpecial = (item) => {
+  return (calculateGoldCost(item) / item.special).toFixed(2);
+};
+</script>
+  
+<style scoped>
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input {
+  width: 100%;
+  padding: 5px;
+}
+
+h2 {
+  margin-top: 20px;
+}
+
+hr {
+  border: 1px solid #ddd;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+</style>
+  
